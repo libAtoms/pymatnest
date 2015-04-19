db31457223a4bbd3fdbc09b1269564d8df3ab9f1
Noam Bernstein <bernstei@tin.(none)>
2015-04-19 17:28:35 -0400

    Start adding abstraction of random stream switching to ns_rng.py.  Need to do julia, and actually use abstraction in ns_Run

--- ns_rng.py
+++ ns_rng.py
@@ -7,6 +7,12 @@ class NsRng:
       raise("normal not implemented")
    def shuffle_in_place(self, list):
       raise("shuffle_in_place not implemented")
+   def seed_init(self):
+      raise("seed_init not implemented")
+   def seed_common(self):
+      raise("seed_common not implemented")
+   def seed_local(self):
+      raise("seed_local not implemented")
 
 import numpy as np
 class NsRngNumpy(NsRng):
@@ -18,6 +24,31 @@ class NsRngNumpy(NsRng):
       return np.random.normal(0.0,std_dev)
    def shuffle_in_place(self, list):
       np.random.shuffle(list)
+   def init_seeds(self, delta_seed, comm):
+      if comm is None:
+	 self.common_random_state = None
+      else:
+	 if rank == 0:
+	    if delta_seed > 0:
+	       np.random.seed(comm.size + 1 + delta_seed)
+	    else:
+	       np.random.seed()
+	    self.common_random_state = np.random.get_state()
+	 else:
+	    self.common_random_state = None
+	 self.common_random_state = comm.bcast(self.common_random_state, root=0)
+      if delta_seed > 0:
+	 np.random.seed(comm.rank + 1 + delta_seed)
+      else:
+	 np.random.seed()
+   def switch_to_common(self):
+      if self.common_random_state is not None:
+	 self.local_random_state = np.random.get_state()
+	 np.random.set_state(self.common_random_state)
+   def switch_to_local(self):
+      if self.common_random_state is not None:
+	 self.common_random_state = np.random.get_state()
+	 np.random.set_state(self.local_random_state)
 
 class NsRngJulia(NsRng):
    def __init__(self, j):
@@ -50,3 +81,28 @@ class NsRngInternal(NsRng):
       return random.normalvariate(0.0, std_dev)
    def shuffle_in_place(self, list):
       random.shuffle(list)
+   def init_seeds(self, delta_seed, comm):
+      if comm is None:
+	 self.common_random_state = None
+      else:
+	 if rank == 0:
+	    if delta_seed > 0:
+	       random.seed(comm.size + 1 + delta_seed)
+	    else:
+	       random.seed()
+	    self.common_random_state = random.get_state()
+	 else:
+	    self.common_random_state = None
+	 self.common_random_state = comm.bcast(self.common_random_state, root=0)
+      if delta_seed > 0:
+	 random.seed(comm.rank + 1 + delta_seed)
+      else:
+	 random.seed()
+   def switch_to_common(self):
+      if self.common_random_state is not None:
+	 self.local_random_state = random.get_state()
+	 random.set_state(self.common_random_state)
+   def switch_to_local(self):
+      if self.common_random_state is not None:
+	 self.common_random_state = random.get_state()
+	 random.set_state(self.local_random_state)
