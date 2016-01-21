19ffad10faf65391089385cc5db51acfcb34df7d
L. Bartok-Partay <lb415@cam.ac.uk>
2016-01-21 11:20:07 +0000

    updated for auto-documentation

--- tests_pymatnest/travis/build_docs.sh
+++ tests_pymatnest/travis/build_docs.sh
@@ -1,26 +1,25 @@
 #! /bin/bash
 
 # packages for building docs
-pip install sphinx nbconvert numpydoc
-
-# Install in the virtualenv
-python setup.py install
-cd ../../../
+pip install sphinx 
 
 # Work from the docs directory
 cd doc
 
 # Put a working copy of the gh-pages where they are expected
-PAGES_DIR=../../pymatnest-pages
+mkdir ../doc_build/
+PAGES_DIR=../doc_build/html
 git clone -b gh-pages ${PAGES_URL} ${PAGES_DIR} > /dev/null 2>&1
 
 # set up git so it can push
 git config --global user.name "Travis-CI"
 git config --global user.email "build@travis.org"
 
-# For some reason, it won't import from the current directory;
-export PYTHONPATH=`pwd`:$PYTHONPATH
-
 # html version is fine, push it later
-make docpush
+make html
+
+cd $PAGES_DIR
+git add .
+git commit -m "update docs"
+git push
 
