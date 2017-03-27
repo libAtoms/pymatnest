b3ba609ace0ddfe679ef47f7557269b6d8072fdc
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2017-03-27 17:06:12 -0400

    Fix bug in ns_run_analysis_polymer.py keeping track of first call to analyzer

--- ns_run_analyzers/ns_run_analysis_polymer.py
+++ ns_run_analyzers/ns_run_analysis_polymer.py
@@ -6,41 +6,35 @@ class NSAnalyzer():
     def __init__(self, comm):
         self.comm = comm
         self.orig_end_to_end_vector = []
-        self.config_counter = 0
+        self.first_time = {}
         return
 
     def find_ends(self, at):
         return (np.reshape(np.where(at.arrays['angles'] == '_')[0],(-1,2)))
 
-    def end_to_end_distances(self, at):
+    def end_to_end_distances(self, at, label):
         dists = []
-        if len(self.orig_end_to_end_vector) == 0:
-            first_time=True
-        else:
-            first_time=False
         for ends in self.find_ends(at):
             cur_pos = np.zeros((3))
             for i_at in range(ends[0], ends[1]):
                 cur_pos += at.get_distance(i_at, i_at+1, mic=True, vector=True)
-            if first_time:
-                self.orig_end_to_end_vector.append(cur_pos/np.linalg.norm(cur_pos))
             dists.append(np.linalg.norm(cur_pos))
 
         return (dists)
 
-    def end_to_end_vector_dots(self, at):
+    def end_to_end_vector_dots(self, at, label):
         vec_dots = []
         for ends in self.find_ends(at):
             cur_pos = np.zeros((3))
             for i_at in range(ends[0], ends[1]):
                 cur_pos += at.get_distance(i_at, i_at+1, mic=True, vector=True)
+            if self.first_time[label]:
+                self.orig_end_to_end_vector.append(cur_pos/np.linalg.norm(cur_pos))
             vec_dots.append(np.dot(self.orig_end_to_end_vector[self.config_counter], cur_pos)/np.linalg.norm(cur_pos))
             self.config_counter += 1
-            if self.config_counter == len(self.orig_end_to_end_vector):
-                self.config_counter = 0
         return (vec_dots)
 
-    def radii_of_gyration(self, at):
+    def radii_of_gyration(self, at, label):
         dists = []
         for ends in self.find_ends(at):
             cur_pos = np.zeros((3))
@@ -60,7 +54,14 @@ class NSAnalyzer():
         return (dists)
 
     def print_quantity_distributions(self, walkers, label, calculator, calculator_label):
-        local_quantities = [calculator(at) for at in walkers]
+        self.config_counter = 0
+        if calculator_label not in self.first_time: # first time
+            self.first_time[calculator_label] = True
+        else:
+            self.first_time[calculator_label] = False
+
+        local_quantities = [calculator(at, calculator_label) for at in walkers]
+
         if self.comm is None:
             global_quantities = local_quantities
         else:
