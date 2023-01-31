010c27b178e15ebad8b52aa66868bb8ac89db67c
Livia Bartok-Partay <lbpartay@gmail.com>
2023-01-31 19:07:55 +0000

    Collapsible sphere potential added (see DOI: 10.1039/c7sm02429k)

---
+++ example_CollapsingSpheres_params_mod.F90
@@ -0,0 +1,8 @@
+module example_CollapsingSpheres_params_mod
+   integer, parameter :: N_Z = 1
+   double precision :: sigma(N_Z)
+   double precision :: sigma1(N_Z)
+   double precision :: kk(N_Z)
+   double precision :: cutoff(N_Z), cutoff_sq(N_Z)
+   double precision :: e_shift(N_Z)
+end module example_CollapsingSpheres_params_mod
